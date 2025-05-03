9a0098158b7ccb7b9b1bd94564c8c33af21c0f46
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2025-05-03 23:21:36 +0600

    Working on updates

--- backend/app/src/test/kotlin/io/tolgee/AuthProviderChangeTest.kt
+++ backend/app/src/test/kotlin/io/tolgee/AuthProviderChangeTest.kt
@@ -22,12 +22,14 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.web.client.RestTemplate
 import java.util.HashMap
 import kotlin.collections.get
 
+@ActiveProfiles("tests")
 class AuthProviderChangeTest : AuthorizedControllerTest() {
   @Autowired
   private lateinit var authProviderChangeService: AuthProviderChangeService
